1a576a3c2c83e7ded6b693d849a241f8e164d5b6
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-04 15:30:39 +0200

    Add numeric_kinds module

--- src/main.f90
+++ src/main.f90
@@ -1,40 +1,39 @@
 ! Module with objective functions for optimization testing
 module objectives
+use numeric_kinds
 implicit none
 contains
 
   function norm_square(x, n)
-    double precision :: x(:)
-    integer :: n
-    double precision :: norm_square
+    real(dp) :: x(:)
+    integer(i4b) :: n
+    real(dp) :: norm_square
     norm_square = sum(x**2)
   end function
 
   function norm_square_grad(x, n)
-    double precision, dimension(:) :: x
-    integer :: n
-
+    real(dp), dimension(:) :: x
+    integer(i4b) :: n
     ! output gradient vector
-    double precision, dimension(:), allocatable :: norm_square_grad 
+    real(dp), dimension(:), allocatable :: norm_square_grad 
     allocate(norm_square_grad(n))
-
     ! derivative of x^2:
     norm_square_grad = 2*x
   end function
 
   function rastrigin(x, n)
-    double precision :: rastrigin
-    double precision, dimension(1) :: x
-    double precision, parameter :: pi = 3.14159265358979
-    integer :: n
+    real(dp) :: rastrigin
+    real(dp) , dimension(1) :: x
+    real(dp) , parameter :: pi = 3.14159265358979
+    integer(i4b) :: n
     rastrigin = 10*n + sum(x**2 - 10.d0*cos(2*pi*x))
   end function
 
   function rastrigin_grad(x, n)
-    integer :: n
-    double precision, dimension(:) :: x
-    double precision, dimension(:), allocatable :: rastrigin_grad
-    double precision, parameter :: pi = 3.14159265358979
+    integer(i4b) :: n
+    real(dp), dimension(:) :: x
+    real(dp), dimension(:), allocatable :: rastrigin_grad
+    real(dp), parameter :: pi = 3.14159265358979
     allocate(rastrigin_grad(n))
     rastrigin_grad = 2.d0*x + 20.d0*pi*sin(2*pi*x)
   end function
@@ -44,29 +43,30 @@ end module
 
 program main
   use objectives
+  use numeric_kinds
   implicit none
 
   external LB2 ! absolutely horrifying
 
   ! variable declarations
-  integer, parameter :: n = 10
-  double precision, dimension(:), allocatable :: x
-  integer :: i = 1
-  integer, parameter :: num_iters = 100
+  integer(i4b), parameter :: n = 10
+  real(dp), dimension(:), allocatable :: x
+  integer(i4b) :: i = 1
+  integer(i4b), parameter :: num_iters = 100
 
   ! constant parameters for LBFGS algorithm
-  double precision :: f
-  double precision :: g(n)
-  integer, parameter :: m = 5 ! number of history points to keep
-  integer, dimension(1) :: iprint(2) ! printing data
-  double precision, parameter :: eps = 1.0d-5 ! don't use any termination criteria
-  double precision, parameter :: xtol = 1.0d-16 ! line search convergence criterion
+  real(dp) :: f
+  real(dp) :: g(n)
+  integer(i4b), parameter :: m = 5 ! number of history points to keep
+  integer(i4b), dimension(1) :: iprint(2) ! printing data
+  real(dp), parameter :: eps = 1.0d-5 ! don't use any termination criteria
+  real(dp), parameter :: xtol = 1.0d-16 ! line search convergence criterion
   logical, parameter :: diagco = .false. ! don't provide diagonal matrices
 
   ! mutable data for LBFGS
-  integer :: iflag = 0 ! error flag
-  double precision :: w(n*(2*m + 1) + 2*m)
-  double precision :: diag(n)
+  integer(i4b) :: iflag = 0 ! error flag
+  real(dp) :: w(n*(2*m + 1) + 2*m)
+  real(dp) :: diag(n)
 
   ! set print specifications
   iprint(1) = -1 ! don't print anything
