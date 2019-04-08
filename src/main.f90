24ddd93963dda9ada422ea475b1d7a4c8f3b99be
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-08 16:15:49 +0200

    Cleanup main.f90

--- src/main.f90
+++ src/main.f90
@@ -50,28 +50,21 @@ program main
   external LB2 ! absolutely horrifying
 
   ! variable declarations
-  integer(i4b), parameter :: n = 10
+  integer(i4b) :: i = 1 ! counter variable
+  integer(i4b), parameter :: n = 10 ! number of dimensions
+  integer(i4b), parameter :: m = 5 ! number of history points to keep
+
+  ! solution array
   real(dp), dimension(:), allocatable :: x
-  integer(i4b) :: i = 1
   integer(i4b), parameter :: num_iters = 99
 
-  ! constant parameters for LBFGS algorithm
+  ! function value, gradient vector 
   real(dp) :: f
   real(dp) :: g(n)
-  integer(i4b), parameter :: m = 5 ! number of history points to keep
-  integer(i4b), dimension(1) :: iprint(2) ! printing data
-  real(dp), parameter :: eps = 1.0d-5 ! don't use any termination criteria
-  real(dp), parameter :: xtol = 1.0d-16 ! line search convergence criterion
-  logical, parameter :: diagco = .false. ! don't provide diagonal matrices
 
   ! mutable data for LBFGS
-  integer(i4b) :: iflag = 0 ! error flag
-  real(dp) :: w(n*(2*m + 1) + 2*m)
-  real(dp) :: diag(n)
-
-  ! set print specifications
-  iprint(1) = -1 ! don't print anything
-  iprint(2) = 0 ! arbitrary since we don't print anything
+  real(dp) :: w(n*(2*m + 1) + 2*m) ! scratchpad
+  real(dp) :: diag(n) ! hessian diagonal
 
   ! allocate solution array
   allocate(x(n))
@@ -85,7 +78,6 @@ program main
   optimize: do i=1, num_iters
     f = norm_square(x, n)
     g = norm_square_grad(x, n)
-    !call lbfgs(n, m, x, f, g, diagco, diag, iprint, eps, xtol, w, iflag)
     call lbfgs_iteration(f, x, g, n, m, diag, w)
     print *, f
   end do optimize
